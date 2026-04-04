4b81c609c23f4da3bce1c151ab29d55a956be50f
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-04-04 14:31:54 +0200

    modified stack stories ui

--- src/stories/Layout/Stack/index.stories.tsx
+++ src/stories/Layout/Stack/index.stories.tsx
@@ -8,15 +8,28 @@ const meta: Meta<typeof Stack> = {
   parameters: {
     layout: "centered",
   },
+  argTypes: {
+    direction: { control: "select", options: ["row", "column"] },
+    align: { control: "select", options: ["start", "center", "end"] },
+    justify: {
+      control: "select",
+      options: ["start", "center", "between", "around"],
+    },
+    gap: { control: ["2", "4", "6", "8", "10"] },
+  },
 };
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Playground: Story = {
+  args: {
+    direction: "column",
+    gap: "4",
+  },
   render: (args) => {
     return (
-      <Stack {...args} className="gap-4 p-4 bg-gray-400">
+      <Stack {...args} className="flex w-150 h-100 p-2 bg-gray-400">
         <Box className="w-25 h-25 bg-red-500"></Box>
         <Box className="w-25 h-25 bg-blue-500"></Box>
         <Box className="w-25 h-25 bg-yellow-500"></Box>
--- src/stories/Layout/Stack/index.tsx
+++ src/stories/Layout/Stack/index.tsx
@@ -1,10 +1,59 @@
 import Box, { type BoxProps } from "../Box/index";
 import { cn } from "../../../utils/index";
-type StackProps = BoxProps;
 
-const Stack = ({ className, ...props }: StackProps) => {
+type StackProps = BoxProps & {
+  direction?: "row" | "column";
+  align?: "start" | "center" | "end";
+  justify?: "start" | "center" | "between" | "around";
+  gap?: "2" | "4" | "6" | "8" | "10";
+};
+
+const directionClasses = {
+  row: "flex-row",
+  column: "flex-col",
+};
+
+const alignClasses = {
+  start: "items-start",
+  center: "items-center",
+  end: "items-end",
+};
+
+const justifyClasses = {
+  start: "justify-start",
+  center: "justify-center",
+  between: "justify-between",
+  around: "justify-around",
+};
+
+const gapClasses = {
+  2: "gap-2",
+  4: "gap-4",
+  6: "gap-6",
+  8: "gap-8",
+  10: "gap-10",
+};
+
+const Stack = ({
+  direction = "column",
+  align,
+  justify,
+  gap = "4",
+  className,
+  ...props
+}: StackProps) => {
   return (
-    <Box className={cn("flex flex-col items-center", className)} {...props} />
+    <Box
+      className={cn(
+        "flex",
+        directionClasses[direction],
+        align && alignClasses[align],
+        justify && justifyClasses[justify],
+        gap && gapClasses[gap],
+        className,
+      )}
+      {...props}
+    />
   );
 };
 
